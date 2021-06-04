d1e1ab5522626f9fa9a94f08c220e73874fc6338
Miguel Jimenez <migang9@gmail.com>
2021-06-04 02:25:58 -0400

    Coordinates included Miguel & Andieyv

--- iss.js
+++ iss.js
@@ -38,5 +38,26 @@ const fetchCoordsByIP = (ip, callback) => {
     callback(null, { latitude, longitude });
   });
 };
+
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
+};
+
 //console.log(fetchMyIP());
-module.exports = { fetchMyIP, fetchCoordsByIP };
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
