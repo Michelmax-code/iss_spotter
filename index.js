991c107fb34f18ab98e8177dfc7a1b33dc04ac7c
Miguel Jimenez <migang9@gmail.com>
2021-06-04 01:53:27 -0400

    Spotter II Miguel $ Andyiev

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fetchMyIP = require('./iss');
-
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+let ip = '';
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!", error);
@@ -7,3 +7,17 @@ fetchMyIP((error, ip) => {
   }
   console.log("It worked! Returned IP:", ip);
 });
+
+fetchCoordsByIP(ip, (error, coords) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log("It worked! Return", coords);
+});
+/*
+//fetchCoordsByIP(fetchMyIP(callback));
+// we call th efuction fetchCoordsByIP to get coordinates by ip address
+// fetchCoordsByIP(ip,callback) {
+
+// }  */
