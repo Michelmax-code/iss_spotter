e92458d4983c2f0c444962506396a380f91ee5f8
Miguel Jimenez <migang9@gmail.com>
2021-06-04 02:25:11 -0400

    Coordinates included Miguel & Andieyv

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 let ip = '';
 fetchMyIP((error, ip) => {
   if (error) {
@@ -15,6 +15,21 @@ fetchCoordsByIP(ip, (error, coords) => {
   }
   console.log("It worked! Return", coords);
 });
+
+//const { fetchISSFlyOverTimes } = require('./iss');
+
+const coords = { latitude: '43.8113', longitude: '-79.188' };
+
+fetchISSFlyOverTimes(coords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
+
+
 /*
 //fetchCoordsByIP(fetchMyIP(callback));
 // we call th efuction fetchCoordsByIP to get coordinates by ip address
